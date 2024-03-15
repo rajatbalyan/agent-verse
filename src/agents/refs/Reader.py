612c077655fb0f66d11d6450ac990ce9f607698e
ShubhamTiwary914 <shubhamtiwary914@gmail.com>
2024-03-15 15:12:30 +0530

    added references in src/agents/refs

--- src/agents/refs/Reader.py
+++ src/agents/refs/Reader.py
@@ -1,17 +1,16 @@
 from ai_engine import UAgentResponse, UAgentResponseType
-
+ 
 class News(Model):
-    news : str = Field("")
+    news : str
+ 
+news_protocol = Protocol("News System")
+ 
 
-Checkup_Protocol = Protocol("News System")
-
-
-@Checkup_Protocol.on_message(model=News, replies = UAgentResponse)
+@news_protocol.on_message(model=News, replies = UAgentResponse)
 async def on_news_request(ctx: Context, sender: str, msg: News):
     final_news = f"News Received from Agent: {msg.news}"
     ctx.logger.info(f"Received news request from {sender} with prompt: {final_news}")
     await ctx.send(sender, UAgentResponse(message = final_news, type = UAgentResponseType.FINAL))
-
-
+ 
 
 agent.include(news_protocol)
