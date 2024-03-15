1a107c087f2488e41537c86def2dbc9355d085f6
Rajat Balyan <balyanrajat1812@gmail.com>
2024-03-15 19:28:01 +0530

    Added Clinics Data

---
+++ src/data/Clinics_Info.py
@@ -0,0 +1,98 @@
+CLINICS_DATA = [
+    {
+        "name": "City Clinic",
+        "specialty": "General Medicine",
+        "available_slots": "3",
+        "status": "open",
+        "latitude": 50.0764,
+        "longitude": 14.4343,
+        "distance": 0.2,
+        "additional_services": "Laboratory Tests",
+        "rating": "4.5 star",
+        "insurance_accepted": "Yes"
+    },
+    {
+        "name": "Sunset Health Center",
+        "specialty": "Dermatology",
+        "available_slots": "2",
+        "status": "open",
+        "latitude": 50.1115,
+        "longitude": 14.5005,
+        "distance": 0.9,
+        "additional_services": "X-ray, Ultrasound",
+        "rating": "4 star",
+        "insurance_accepted": "Yes"
+    },
+    {
+        "name": "Green Valley Clinic",
+        "specialty": "Pediatrics",
+        "available_slots": "4",
+        "status": "open",
+        "latitude": 50.0755,
+        "longitude": 14.4378,
+        "distance": 3,
+        "additional_services": "Vaccination",
+        "rating": "5 star",
+        "insurance_accepted": "Yes"
+    },
+    {
+        "name": "Pinecrest Medical Center",
+        "specialty": "Ophthalmology",
+        "available_slots": "2",
+        "status": "open",
+        "latitude": 50.0887,
+        "longitude": 14.4018,
+        "distance": 4,
+        "additional_services": "Eye Surgery",
+        "rating": "4.5 star",
+        "insurance_accepted": "Yes"
+    },
+    {
+        "name": "Riverbend Dental Clinic",
+        "specialty": "Dentistry",
+        "available_slots": "1",
+        "status": "open",
+        "latitude": 50.0924,
+        "longitude": 14.4164,
+        "distance": 2.5,
+        "additional_services": "Teeth Whitening",
+        "rating": "4.8 star",
+        "insurance_accepted": "Yes"
+    },
+    {
+        "name": "Hillside Veterinary Clinic",
+        "specialty": "Veterinary Medicine",
+        "available_slots": "3",
+        "status": "open",
+        "latitude": 50.0998,
+        "longitude": 14.4189,
+        "distance": 3.2,
+        "additional_services": "Pet Surgery",
+        "rating": "4.2 star",
+        "insurance_accepted": "Yes"
+    },
+    {
+        "name": "Aurora Mental Health Center",
+        "specialty": "Psychiatry",
+        "available_slots": "2",
+        "status": "open",
+        "latitude": 50.0981,
+        "longitude": 14.4147,
+        "distance": 3.5,
+        "additional_services": "Counseling",
+        "rating": "4.6 star",
+        "insurance_accepted": "Yes"
+    },
+    {
+        "name": "Oakwood Physiotherapy Clinic",
+        "specialty": "Physiotherapy",
+        "available_slots": "3",
+        "status": "open",
+        "latitude": 50.0962,
+        "longitude": 14.4131,
+        "distance": 3.7,
+        "additional_services": "Rehabilitation",
+        "rating": "4.3 star",
+        "insurance_accepted": "Yes"
+    }
+]
